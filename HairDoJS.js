24e8cc6da00974599ff647f213ea648bd586ca39
svanchiro <serena.vanchiro@gmail.com>
2014-08-13 13:20:48 -0400

    Condtionals
    Made all of the conditionals for the different hairstyle outcomes.

--- HairDoJS.js
+++ HairDoJS.js
@@ -1,1 +1,55 @@
-HairDoJS.js
+if (short, curly, a) {
+	alert ("You did it 1")
+}	else if (short, curly, b) {
+		alert ("you did it 2")
+} 	else if (short, curly, c) {
+		alert ("you did it 3")
+} 	else if (short, wavy, a) {
+		alert ("you did it 1")
+} 	else if (short, wavy, b) {
+		alert ("you did it 2")
+} 	else if (short, wavy, c) {
+		alert ("you did it 3")
+}	 else if (short, straight, a) {
+		alert ("you did it 1")
+}	 else if (short, straight, b) {
+		alert ("you did it 2")
+}	 else if (short, straight, c) {
+		alert ("you did it 3")
+} 	else if (med, curly, a) {
+		alert ("you did it 1")
+}	 else if (med, curly, b) {
+		alert ("you did it 2")
+} 	else if (med, curly, c) {
+		alert ("you did it 3")
+} 	else if (med, wavy, a) {
+		alert ("you did it 1")
+} 	else if (med, wavy, b) {
+		alert ("you did it 2")
+} 	else if (med, wavy, c) {
+		alert ("you did it 3")
+} 	else if (med, straight, a) {
+		alert ("you did it 1")
+} 	else if (med, straight, b) {
+		alert ("you did it 2")
+} 	else if (med, straight, c) {
+		alert ("you did it 3")
+} 	else if (long, curly, a) {
+		alert ("you did it 1")
+} 	else if (long, curly, b) {
+		alert ("you did it 2")
+} 	else if (long, curly, c) {
+		alert ("you did it 3")
+}	else if (long, wavy, a) {
+		alert ("you did it 1")
+} 	else if (long, wavy, b) {
+		alert ("you did it 2")
+} 	else if (long, wavy, c) {
+		alert ("you did it 2")
+} 	else if (long, straight, a) {
+		alert ("you did it 1")
+} 	else if (long, straight, b) {
+		alert ("you did it 2")
+} 	else if (long, straight, c) {
+		alert ("you did it 3")
+} 
