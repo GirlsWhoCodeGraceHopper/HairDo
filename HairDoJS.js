c482cdc585d9fb68653c0ba75afd0808942cdaba
fwilder <fwilder327@gmail.com>
2014-08-13 16:12:01 -0400

    updatedjava

--- HairDoJS.js
+++ HairDoJS.js
@@ -1,1 +1,89 @@
-HairDoJS.js
+if (short, curly, a) {
+	alert ("You did it 1")
+}	else if (short, curly, b) {
+		alert ("you did it 2")
+} 	else if (short, curly, c) {
+		alert ("you did it 3")
+} 	else if (short, wavy, a) {
+		alert ("you did it 1")
+} 	else if (short, wavy, b) {
+		alert ("you did it 2")
+} 	else if (short, wavy, c) {
+		alert ("you did it 3")
+}	 else if (short, straight, a) {
+		alert ("you did it 1")
+}	 else if (short, straight, b) {
+		alert ("you did it 2")
+}	 else if (short, straight, c) {
+		alert ("you did it 3")
+} 	else if (med, curly, a) {
+		alert ("you did it 1")
+}	 else if (med, curly, b) {
+		alert ("you did it 2")
+} 	else if (med, curly, c) {
+		alert ("you did it 3")
+} 	else if (med, wavy, a) {
+		alert ("you did it 1")
+} 	else if (med, wavy, b) {
+		alert ("you did it 2")
+} 	else if (med, wavy, c) {
+		alert ("you did it 3")
+} 	else if (med, straight, a) {
+		alert ("you did it 1")
+} 	else if (med, straight, b) {
+		alert ("you did it 2")
+} 	else if (med, straight, c) {
+		alert ("you did it 3")
+} 	else if (long, curly, a) {
+		alert ("you did it 1")
+} 	else if (long, curly, b) {
+		alert ("you did it 2")
+} 	else if (long, curly, c) {
+		alert ("you did it 3")
+}	else if (long, wavy, a) {
+		alert ("you did it 1")
+} 	else if (long, wavy, b) {
+		alert ("you did it 2")
+} 	else if (long, wavy, c) {
+		alert ("you did it 2")
+} 	else if (long, straight, a) {
+		alert ("you did it 1")
+} 	else if (long, straight, b) {
+		alert ("you did it 2")
+} 	else if (long, straight, c) {
+		alert ("you did it 3")
+} 
+ //checkboxes jj
+
+				function newFunction(){
+					if (document.getElementById('a').checked) {
+						length = document.getElementById('a').value;
+						document.getElementById("hi").innerHTML = length;
+							}
+					if (document.getElementById('b').checked) {
+						length = document.getElementById('b').value;
+						document.getElementById("hi").innerHTML = length;
+							}
+					if (document.getElementById('c').checked) {
+						length = document.getElementById('c').value;
+						document.getElementById("hi").innerHTML = length;
+							}
+										
+					if (document.getElementById('a').checked) {
+						type = document.getElementById('e').value;
+						document.getElementById("bye").innerHTML = type;
+							}
+					if (document.getElementById('b').checked) {
+						type = document.getElementById('f').value;
+						document.getElementById("bye").innerHTML = type;
+							}
+					if (document.getElementById('c').checked) {
+						type = document.getElementById('g').value;
+						document.getElementById("bye").innerHTML = type;
+							}
+				
+				
+				}
+			
+			
+
