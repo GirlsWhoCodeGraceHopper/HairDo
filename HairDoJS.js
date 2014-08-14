ee7bce26db444b3256ed1167afcfb4e634b6b8cd
svanchiro <serena.vanchiro@gmail.com>
2014-08-14 19:32:27 -0400

    Added video function
    can embed youtube tutorials to pictures

--- HairDoJS.js
+++ HairDoJS.js
@@ -14,11 +14,18 @@ function show_image(imglink) {
 	//clear the picture div
 	$("#picture").html("");
 	//append img id 
-	$("#picture").append('<img src="' + imglink + '" />');
+	$("#picture").append('<img width="350" height="350" align="right" src="' + imglink + '" />');
 }
 
+function show_video(vidlink) {
+	alert("adding tutorial!");
+	$("#video").html("");
+	//appending vid id
+	$("#video").append('<embed width="560" height="315" src="' + vidlink + '" >');
+	} 
+	
 function newFunction() {
-    var humidity = 41;
+    var humidity = 11;
 
 	//show_image("https://lh6.googleusercontent.com/f0Afo_malHBKaZClIb4w-8X8yQ69mUnmpKw9wJSycTZNbxkEwPQaVmrIfjni1a_vrtE9Gw=s190");
 	//show_image("https://lh5.googleusercontent.com/5wR_2UTjEdyiHBsGGUZMGHQE-SO0gifOCXNI7MWTZQ6mW5iON3jSbNTnwahsoAtSe8FShQ=w1034-h627");
@@ -30,22 +37,27 @@ function newFunction() {
             document.getElementById("hi").innerHTML = length;
             type = document.getElementById('e').value;
             document.getElementById("bye").innerHTML = type;
+			alert("hey");
 			show_image("http://images.beautyriot.com/photos/galleries/easy-styles-curly-hair-03.jpg");
+			alert("sup yo");
+			show_video("http://www.youtube.com/embed/m6JCf320OBQ");
 			}
 		if (document.getElementById('a').checked && document.getElementById('f').checked) {
-                length = document.getElementById('a').value;
-                document.getElementById("hi").innerHTML = length;
-                type = document.getElementById('f').value;
-                document.getElementById("bye").innerHTML = type;
-                show_image("http://www.short-haircut.com/wp-content/uploads/2014/12/Short-Haircuts-Wavy-Hair.jpg");
-                //alert("Your hair is short");
+            length = document.getElementById('a').value;
+            document.getElementById("hi").innerHTML = length;
+            type = document.getElementById('f').value;
+            document.getElementById("bye").innerHTML = type;
+            show_image("http://www.short-haircut.com/wp-content/uploads/2014/12/Short-Haircuts-Wavy-Hair.jpg");
+            //alert("Your hair is short");
+			show_video("http://www.youtube.com/v/SJuB03Zg614");
             }
 		if (document.getElementById('a').checked && document.getElementById('g').checked) {
-                length = document.getElementById('a').value;
-                document.getElementById("hi").innerHTML = length;
-                type = document.getElementById('g').value;
-                document.getElementById("bye").innerHTML = type;
-                show_image("http://www.short-haircut.com/wp-content/uploads/2014/12/20-Haircuts-for-Short-Wavy-Hair_9.jpg");
+            length = document.getElementById('a').value;
+            document.getElementById("hi").innerHTML = length;
+            type = document.getElementById('g').value;
+            document.getElementById("bye").innerHTML = type;
+            show_image("http://www.short-haircut.com/wp-content/uploads/2014/12/20-Haircuts-for-Short-Wavy-Hair_9.jpg");
+			show_video("http://www.youtube.com/v/s6Bt0eukkkk?version=3&amp;hl=en_US");
                 //alert("Your hair is short");
             }	
 		if (document.getElementById('b').checked && document.getElementById('e').checked) {
@@ -100,7 +112,7 @@ function newFunction() {
                 document.getElementById("hi").innerHTML = length;
                 type = document.getElementById('e').value;
                 document.getElementById("bye").innerHTML = type;
-                show_image("http://media-cache-ak0.pinimg.com/236x/76/1b/f7/761bf7e5342baef66c8672b9d159dfa8.jpg)")
+                show_image ("http://media-cache-ak0.pinimg.com/236x/76/1b/f7/761bf7e5342baef66c8672b9d159dfa8.jpg");
                 //alert("Your hair is short");
             }
         if (document.getElementById('a').checked && document.getElementById('f').checked) {
@@ -117,8 +129,51 @@ function newFunction() {
                 type = document.getElementById('g').value;
                 document.getElementById("bye").innerHTML = type;
                 show_image("http://www.short-haircut.com/wp-content/uploads/2014/07/20-Straight-Hairstyles-for-Short-Hair_4.jpg");
-
-                //alert("Your hair is short");
             }
-			}
+		if (document.getElementById('b').checked && document.getElementById('e').checked) {
+            length = document.getElementById('b').value;
+            document.getElementById("hi").innerHTML = length;
+            type = document.getElementById('e').value;
+            document.getElementById("bye").innerHTML = type;
+            show_image("http://www.hairstylesco.com/wp-content/uploads/2014/01/Volumized-Short-Hairstyles-for-Curly-Hair.jpg");
+            alert("Your hair is mediumlength");
+            }
+        if (document.getElementById('b').checked && document.getElementById('f').checked) {
+             length = document.getElementById('b').value;
+             document.getElementById("hi").innerHTML = length;
+             type = document.getElementById('f').value;
+             document.getElementById("bye").innerHTML = type;
+             show_image("http://images.beautyriot.com/photos/galleries/easy-styles-curly-hair-keri-russell-01.jpg");
+             alert("Your hair is mediumlength");
+            }
+        if (document.getElementById('b').checked && document.getElementById('g').checked) {
+             length = document.getElementById('b').value;
+             document.getElementById("hi").innerHTML = length;
+             type = document.getElementById('g').value;
+             document.getElementById("bye").innerHTML = type;
+             show_image("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTRzjlSkM4IbvzNOW3OAap1-rU5i5QwyhE1IwlRszbVeL6Io9358Q");
+             alert("Your hair is mediumlength");
+            }
+        if (document.getElementById('c').checked && document.getElementById('e').checked) {
+             length = document.getElementById('c').value;
+             document.getElementById("hi").innerHTML = length;
+             type = document.getElementById('e').value;
+             document.getElementById("bye").innerHTML = type;
+             show_image("http://www.yayhairstyles.com/wp-content/uploads/2014/08/homecoming-hairstyles-half-up-half-down-curly-updo-hairstyles-for-prom-53e5b00c1e7f3-500x500.jpg");
+            }
+        if (document.getElementById('c').checked && document.getElementById('f').checked) {
+             length = document.getElementById('c').value;
+             document.getElementById("hi").innerHTML = length;
+             type = document.getElementById('f').value;
+             document.getElementById("bye").innerHTML = type;
+             show_image("http://www.viphairstyles.com/wp-content/uploads/2013/11/Reddish-Brown-Highlights.jpg");
+            }
+        if (document.getElementById('c').checked && document.getElementById('g').checked) {
+             length = document.getElementById('c').value;
+             document.getElementById("hi").innerHTML = length;
+             type = document.getElementById('g').value;
+             document.getElementById("bye").innerHTML = type;
+             show_image("http://www.womanhaircolor.com/hairstyles/side-swept-hairstyles-2014_6.jpg");
+            }		
+}
 }
